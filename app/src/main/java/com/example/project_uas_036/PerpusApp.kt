14612a92c3c799122c5887ef803c1e28def68e7e
muhfarrasm <muhammadfarrasm@gmail.com>
2025-01-26 23:38:49 +0700

    ADD -- PerpusApp kelas aplikasi utama Anda yang mewarisi Application dalam Android

--- app/src/main/java/com/example/project_uas_036/PerpusApp.kt
+++ app/src/main/java/com/example/project_uas_036/PerpusApp.kt
@@ -1,4 +1,17 @@
 package com.example.project_uas_036
 
-class PerpusApp {
+import android.app.Application
+import com.example.project_uas_036.dependenciesinjection.AppContainer
+import com.example.project_uas_036.dependenciesinjection.BukuContainer
+
+class PerpusApp : Application(){
+
+    lateinit var bukcontainer: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        bukcontainer = BukuContainer()
+
+    }
+
 }
