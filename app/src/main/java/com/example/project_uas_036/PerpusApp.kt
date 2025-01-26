9d4f89537d778318119196c55b0bf621e8283137
muhfarrasm <muhammadfarrasm@gmail.com>
2025-01-27 01:47:46 +0700

    ADD -- Menambahkan  inisialisasi  Penulis

--- app/src/main/java/com/example/project_uas_036/PerpusApp.kt
+++ app/src/main/java/com/example/project_uas_036/PerpusApp.kt
@@ -3,18 +3,22 @@ package com.example.project_uas_036
 import android.app.Application
 import com.example.project_uas_036.dependenciesinjection.AppContainer
 import com.example.project_uas_036.dependenciesinjection.AppKategoriContainer
+import com.example.project_uas_036.dependenciesinjection.AppPenulisContainer
 import com.example.project_uas_036.dependenciesinjection.BukuContainer
 import com.example.project_uas_036.dependenciesinjection.KategoriContainer
+import com.example.project_uas_036.dependenciesinjection.PenulisContainer
 
 class PerpusApp : Application(){
 
     lateinit var bukcontainer: AppContainer
     lateinit var katcontainer: AppKategoriContainer
+    lateinit var penuliscontainer: AppPenulisContainer
 
     override fun onCreate() {
         super.onCreate()
         bukcontainer = BukuContainer()
         katcontainer = KategoriContainer()
+        penuliscontainer = PenulisContainer()
 
 
 
