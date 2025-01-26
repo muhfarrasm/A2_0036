5d9b54f7447b89180fc9c82ab9b839d65b056967
muhfarrasm <muhammadfarrasm@gmail.com>
2025-01-26 23:37:47 +0700

    ADD -- HomeBukuView komponen untuk mengatur UI

---
+++ app/src/main/java/com/example/project_uas_036/ui/view/Buku/HomeBukuView.kt
@@ -0,0 +1,336 @@
+package com.example.project_uas_036.ui.view.Buku
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.project_uas_036.R
+import com.example.project_uas_036.model.Buku
+import com.example.project_uas_036.ui.PenyediaViewModel
+import com.example.project_uas_036.ui.customwidget.CoustumeTopAppBar
+import com.example.project_uas_036.ui.navigation.DestinasiNavigasi
+import com.example.project_uas_036.ui.viewmodel.Buku.HomeBukuViewModel
+import com.example.project_uas_036.ui.viewmodel.Buku.HomeUiState
+
+object DestinasiHomeBuku : DestinasiNavigasi {
+    override val route = "homebook"
+    override val titleRes = "Home Book"
+}
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeBukuScreen(
+    navigateToltemEntry: () -> Unit,
+
+    navigateToHomeKategori: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+    viewModel: HomeBukuViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+
+            Column{
+
+                Spacer(modifier = Modifier.height(21.dp))
+                Header(
+                    namaApp = "Ayo Membaca",
+                    ID = R.drawable.fae282ab40e04da8eed627a767815dc8
+                )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 4.dp, vertical = 2.dp),
+                    horizontalArrangement = Arrangement.spacedBy(2.dp)
+                ) {
+                    Button(
+                        onClick = navigateToltemEntry,
+                        shape = MaterialTheme.shapes.small,
+                    ) {
+                        Text(text = "Tambah Buku")
+                    }
+
+
+                    Button(
+                        onClick = navigateToHomeKategori, // Meneruskan parameter navigateToDetailKategori
+                        shape = MaterialTheme.shapes.small,
+                    ) {
+                        Text(text = "Menu kategori")
+                    }
+
+
+                }
+                CoustumeTopAppBar(
+                    title = DestinasiHomeBuku.titleRes,
+                    canNavigateBack = false,
+                    scrollBehavior = scrollBehavior,
+                    onRefresh = {
+                        viewModel.getBuku()
+                    }
+                )
+            }
+
+        },
+
+
+//        floatingActionButton = {
+//            FloatingActionButton(
+//                onClick = navigateToltemEntry,
+//                shape = MaterialTheme.shapes.medium,
+//                modifier = Modifier.padding(18.dp)
+//            ){
+//                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Kontak")
+//            }
+//        },
+    ){ innerPadding ->
+        HomeStatus(
+            homeUiState = viewModel.BookUiState,
+            retryAction = { viewModel.getBuku() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick, onDeleteClick = {
+                viewModel.deleteBuku(it.id_buku)
+                viewModel.getBuku()
+            }
+        )
+    }
+}
+@Composable
+fun Header(
+    namaApp: String,
+    ID: Int
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                brush = Brush.horizontalGradient(
+                    colors = listOf(
+                        Color(0xFF6700ff),
+                        Color(0xFF6700ff)
+                    )
+                )
+            )
+            .padding(16.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(RoundedCornerShape(40.dp))
+                    .background(Color.White.copy(alpha = 0.3f))
+            ) {
+                Image(
+                    painter = painterResource(ID),
+                    contentDescription = "profil",
+                    modifier = Modifier
+                        .size(70.dp)
+                        .clip(RoundedCornerShape(35.dp))
+                        .align(Alignment.Center),
+                    contentScale = ContentScale.Crop
+                )
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Column {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = namaApp,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Icon(
+                        imageVector = Icons.Default.Home,
+                        contentDescription = "ikon",
+                        tint = Color.White
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+
+            }
+        }
+    }
+}
+@Composable
+fun HomeStatus(
+    homeUiState: HomeUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit,
+    onDeleteClick: (Buku) -> Unit = {},
+){
+    when (homeUiState) {
+        is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomeUiState.Success ->
+            if (homeUiState.buku.isEmpty()) {
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Kontak")
+                }
+            } else {
+                BukuLayout(
+                    buku = homeUiState.buku,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.id_buku)
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.fae282ab40e04da8eed627a767815dc8),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )  {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = ""
+        )
+    }
+}
+
+@Composable
+fun BukuLayout(
+    buku: List<Buku>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Buku) -> Unit,
+    onDeleteClick: (Buku) -> Unit = {},
+){
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(buku){kontak ->
+            bukuCard(
+                buku = kontak,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        onDetailClick(kontak)},
+                onDeleteClick = {
+                    onDeleteClick(kontak)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun bukuCard(
+    buku: Buku,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Buku) -> Unit = {}
+) {
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = buku.id_buku,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = { onDeleteClick(buku) }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null,
+                    )
+                }
+                Text(
+                    text = buku.nama_buku,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Text(
+                text = buku.tanggal_terbit,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = buku.status_buku,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
