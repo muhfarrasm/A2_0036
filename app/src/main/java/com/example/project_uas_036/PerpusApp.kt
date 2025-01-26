69fedb1e5d6a7d6bd845253721ef47b8c54c2737
muhfarrasm <muhammadfarrasm@gmail.com>
2025-01-27 01:11:41 +0700

    ADD -- Menambahkan  inisial Kategori

--- app/src/main/java/com/example/project_uas_036/PerpusApp.kt
+++ app/src/main/java/com/example/project_uas_036/PerpusApp.kt
@@ -2,15 +2,21 @@ package com.example.project_uas_036
 
 import android.app.Application
 import com.example.project_uas_036.dependenciesinjection.AppContainer
+import com.example.project_uas_036.dependenciesinjection.AppKategoriContainer
 import com.example.project_uas_036.dependenciesinjection.BukuContainer
+import com.example.project_uas_036.dependenciesinjection.KategoriContainer
 
 class PerpusApp : Application(){
 
     lateinit var bukcontainer: AppContainer
+    lateinit var katcontainer: AppKategoriContainer
 
     override fun onCreate() {
         super.onCreate()
         bukcontainer = BukuContainer()
+        katcontainer = KategoriContainer()
+
+
 
     }
 
