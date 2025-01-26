16d167d5db678c5af0a8cf381980b84d71889f03
muhfarrasm <muhammadfarrasm@gmail.com>
2025-01-26 23:56:40 +0700

    ADD -- Menambahkan initializer untuk Insert, Detail, dan Update Buku

--- app/src/main/java/com/example/project_uas_036/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/project_uas_036/ui/PenyediaViewModel.kt
@@ -3,16 +3,23 @@ package com.example.project_uas_036.ui
 import android.os.Build
 import androidx.annotation.RequiresExtension
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.project_uas_036.PerpusApp
+import com.example.project_uas_036.ui.viewmodel.Buku.DetailBukuViewModel
 import com.example.project_uas_036.ui.viewmodel.Buku.HomeBukuViewModel
+import com.example.project_uas_036.ui.viewmodel.Buku.InsertBukuViewModel
+import com.example.project_uas_036.ui.viewmodel.Buku.UpdateBukuViewModel
 
 object PenyediaViewModel {
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     val Factory = viewModelFactory {
         initializer { HomeBukuViewModel(aplikasiKontak().bukcontainer.kontakRepository) }
+        initializer { InsertBukuViewModel(aplikasiKontak().bukcontainer.kontakRepository) }
+        initializer { DetailBukuViewModel(createSavedStateHandle(),aplikasiKontak().bukcontainer.kontakRepository) }
+        initializer { UpdateBukuViewModel(createSavedStateHandle(),aplikasiKontak().bukcontainer.kontakRepository) }
 
     }
 }
--- app/src/main/java/com/example/project_uas_036/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/project_uas_036/ui/navigation/PengelolaHalaman.kt
@@ -5,13 +5,19 @@ import androidx.annotation.RequiresExtension
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.project_uas_036.ui.view.Buku.DestinasiDetailBuku
 import com.example.project_uas_036.ui.view.Buku.DestinasiHomeBuku
 import com.example.project_uas_036.ui.view.Buku.DestinasiTambahBuku
+import com.example.project_uas_036.ui.view.Buku.DestinasiUpdateBuku
+import com.example.project_uas_036.ui.view.Buku.DetailBukuViewScreen
+import com.example.project_uas_036.ui.view.Buku.EntryBukuScreen
 import com.example.project_uas_036.ui.view.Buku.HomeBukuScreen
+import com.example.project_uas_036.ui.view.Buku.UpdateBukuScreen
 
 @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 @Composable
@@ -31,10 +37,61 @@ fun PengelolaHalaman(
                     // Navigasi ke Detail Buku dengan ID Buku sebagai parameter
                     navController.navigate("${DestinasiDetailBuku.route}/$idBuku")
                 },
-                navigateToHomeKategori = {
-                    navController.navigate(DestinasiHomeKategori.route)
-                },
+
             )
         }
+
+        // Input Data (Tambah Buku)
+        composable(DestinasiTambahBuku.route) {
+            EntryBukuScreen(navigateBack = {
+                navController.navigate(DestinasiHomeBuku.route) {
+                    // Menghapus semua halaman sebelumnya agar back ke Home langsung ke halaman utama
+                    popUpTo(DestinasiHomeBuku.route) { inclusive = true }
+                }
+            })
+        }
+
+        // Detail Buku (menerima ID buku sebagai argument)
+        composable(
+            DestinasiDetailBuku.routesWithArg,
+            arguments = listOf(navArgument(DestinasiDetailBuku.id_buku) {
+                type = NavType.StringType // Pastikan tipe argument sesuai
+            })
+        ) {
+            val idBuku = it.arguments?.getString(DestinasiDetailBuku.id_buku)
+            idBuku?.let { id_buku ->
+                DetailBukuViewScreen(
+                    navigateToItemUpdate = {
+                        // Navigasi ke Update Buku dengan ID Buku sebagai parameter
+                        navController.navigate("${DestinasiUpdateBuku.route}/$id_buku")
+                    },
+                    navigateBack = {
+                        navController.navigate(DestinasiHomeBuku.route) {
+                            // Kembali ke halaman Home
+                            popUpTo(DestinasiHomeBuku.route) { inclusive = true }
+                        }
+                    },
+
+                )
+            }
+        }
+
+
+        // Update Buku (menerima ID buku sebagai argument)
+        composable(
+            DestinasiUpdateBuku.routesWithArg,
+            arguments = listOf(navArgument(DestinasiUpdateBuku.id_buku) {
+                type = NavType.StringType // Pastikan tipe argument sesuai
+            })
+        ) {
+            val idBuku = it.arguments?.getString(DestinasiUpdateBuku.id_buku)
+            idBuku?.let { idbook ->
+                UpdateBukuScreen(
+                    onBack = { navController.popBackStack() },
+                    onNavigate = { navController.popBackStack() }
+                )
+            }
+        }
+
     }
 }
