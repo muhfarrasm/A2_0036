b184dbbc60b42a9122e0236c5ec319ad6bc7b3e2
muhfarrasm <muhammadfarrasm@gmail.com>
2025-01-27 01:11:59 +0700

    ADD -- Menambahkan  route Kategori

--- app/src/main/java/com/example/project_uas_036/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/project_uas_036/ui/navigation/PengelolaHalaman.kt
@@ -1,6 +1,7 @@
 package com.example.project_uas_036.ui.navigation
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresExtension
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,6 +19,14 @@ import com.example.project_uas_036.ui.view.Buku.DetailBukuViewScreen
 import com.example.project_uas_036.ui.view.Buku.EntryBukuScreen
 import com.example.project_uas_036.ui.view.Buku.HomeBukuScreen
 import com.example.project_uas_036.ui.view.Buku.UpdateBukuScreen
+import com.example.project_uas_036.ui.view.Kategori.DestinasiDetailKategori
+import com.example.project_uas_036.ui.view.Kategori.DestinasiHomeKategori
+import com.example.project_uas_036.ui.view.Kategori.DestinasiTambahKategori
+import com.example.project_uas_036.ui.view.Kategori.DestinasiUpdateKategori
+import com.example.project_uas_036.ui.view.Kategori.DetailKategoriViewScreen
+import com.example.project_uas_036.ui.view.Kategori.EntryKategoriScreen
+import com.example.project_uas_036.ui.view.Kategori.HomeKategoriScreen
+import com.example.project_uas_036.ui.view.Kategori.UpdateKategoriScreen
 
 @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 @Composable
@@ -37,6 +46,7 @@ fun PengelolaHalaman(
                     // Navigasi ke Detail Buku dengan ID Buku sebagai parameter
                     navController.navigate("${DestinasiDetailBuku.route}/$idBuku")
                 },
+                navigateToHomeKategori = { navController.navigate(DestinasiHomeKategori.route)}
 
             )
         }
@@ -91,6 +101,71 @@ fun PengelolaHalaman(
                     onNavigate = { navController.popBackStack() }
                 )
             }
+        }
+
+        //          KATEGORI
+
+        // Home Screen Kategori
+        composable(DestinasiHomeKategori.route) {
+            HomeKategoriScreen(
+                navigateToKategoriEntry = { navController.navigate(DestinasiTambahKategori.route) },
+                navigateBack = { navController.popBackStack() },
+                onDetailClick = { idkategori ->
+                    navController.navigate("${DestinasiDetailKategori.route}/$idkategori")
+                }
+            )
+        }
+
+        // Input Data (Tambah Kategori)
+        composable(DestinasiTambahKategori.route) {
+            EntryKategoriScreen(navigateBack = {
+                navController.navigate(DestinasiHomeKategori.route) {
+                    // Menghapus semua halaman sebelumnya agar back ke Home langsung ke halaman utama
+                    popUpTo(DestinasiHomeKategori.route) { inclusive = true }
+                }
+            })
+        }
+
+        // Detail Kategori (menerima ID kategori sebagai argument)
+        composable(
+            DestinasiDetailKategori.routesWithArg,
+
+            arguments = listOf(navArgument(DestinasiDetailKategori.idkategori) {
+                type = NavType.StringType // Pastikan tipe argument sesuai
+            })
+        ) {
+            val idKategori = it.arguments?.getString(DestinasiDetailKategori.idkategori)
+            idKategori?.let { idkategori ->
+                DetailKategoriViewScreen(
+                    navigateToKategoriUpdate = {
+                        Log.d("DetailKategori", "Navigating to update kategori: $idkategori")
+                        navController.navigate("${DestinasiUpdateKategori.route}/$idkategori")
+                    },
+                    navigateBack = {
+                        navController.navigate(DestinasiHomeKategori.route) {
+                            // Kembali ke halaman Home
+                            popUpTo(DestinasiHomeKategori.route) { inclusive = true }
+                        }
+                    }
+                )
+            }
+        }
+
+        // Update Kategori
+        composable(
+            DestinasiUpdateKategori.routesWithArg,
+            arguments = listOf(navArgument(DestinasiUpdateKategori.id_kategori) {
+                type = NavType.StringType // Pastikan tipe argument sesuai
+            })
+        ) {
+            Log.d("Navigasi", "Masuk ke rute dengan idKategori: ${it.arguments?.getString(DestinasiUpdateKategori.id_kategori)}")
+            val idkategori = it.arguments?.getString(DestinasiUpdateKategori.id_kategori)
+            idkategori?.let { idkat ->
+                UpdateKategoriScreen(
+                    onBack = { navController.popBackStack() },
+                    onNavigate = { navController.popBackStack() }
+                )
+            }?: Log.e("Navigasi", "idKategori tidak diterima dengan benar!")
         }
 
     }
